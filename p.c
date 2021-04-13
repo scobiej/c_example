b47879470443d8cf866b54ebc44b1f90dd9e6e45
luke.jones@ada.ac.uk <luke.jones@ada.ac.uk>
2021-04-13 12:18:24 +0100

    test

--- p.c
+++ p.c
@@ -10,19 +10,13 @@ int main()
 	if (data != NULL) {
 
 		for (int i=0; i<10; i++) {
+			*(data + i) = 60 + i;
+			printf("data = %d\n", *(data+i));
+			
 
 		}
 		
-		*data = 60;
-		printf("data = %d\n", *data);
-
-		*(data+1) = 61;
-
-		printf("data = %d\n", *(data+1));
-		printf("data = %d\n", *(data+2012));
-
-		*(data+2) = 62;
-
+		
 		free(data);
 	} else {
 		printf("BIG TROUBLE\n");
